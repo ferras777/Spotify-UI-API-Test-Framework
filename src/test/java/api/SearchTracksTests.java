a2e0079df77f918f430ebc848e7bc931a50cb82d
Ilya_Malykh <ferras2007@yandex.ru>
2020-10-05 15:57:42 +0300

    Add depend

--- src/test/java/api/SearchTracksTests.java
+++ src/test/java/api/SearchTracksTests.java
@@ -22,7 +22,7 @@ public class SearchTracksTests {
         Utils.schemaValidation(response, "SearchTrackSchema.json");
     }
 
-    @Test(description="Search track")
+    @Test(description="Search track", dependsOnMethods = {"searchTrackSchemaValidation"})
     public void searchTrack() {
         SearchTrackBody searchTrackBody = response.as(SearchTrackBody.class);
 
