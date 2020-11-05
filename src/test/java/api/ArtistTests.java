2abc2d99f1b512e5c6a6f370b6d2cd81741a0e2e
Ilya_Malykh <ferras2007@yandex.ru>
2020-11-05 15:05:56 +0300

    Delete listener

--- src/test/java/api/ArtistTests.java
+++ src/test/java/api/ArtistTests.java
@@ -2,14 +2,12 @@ package api;
 
 import api.bodies.artist.ArtistBody;
 import api.bodies.json.JsonData;
-import api.listeners.Listener;
 import api.requests.ArtistRequests;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Stories;
 import io.qameta.allure.Story;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import java.io.FileNotFoundException;
@@ -18,7 +16,6 @@ import static api.asserts.ArtistApiAssertions.checkResponseRightArtistId;
 import static api.asserts.ArtistApiAssertions.checkResponseRightArtistName;
 import static api.utils.Json.getArtistsDataFromJson;
 
-@Listeners(Listener.class)
 public class ArtistTests {
 
     ArtistRequests artistRequests = new ArtistRequests();
